5f351baeb59f50cd62605e6b6f825ac15f411015
Marc Edison <suanieeee@yahoo.com>
2023-11-03 19:53:06 +0800

    Home test done

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import './styles.css'
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
-            <div className="container">
-                <div className="left">
-                    This is Left
+            <section className="section-1">
+                <div className="container">
+                    <div className="title">
+                        Are you tired of going to office?
+                    </div>
                 </div>
-            </div>
+            </section>
+
+            <section className="section-2">
+                <div className="container">
+                    <div className="title">
+                       We Introduce,
+                    </div>
+                    <div className="desc">
+                        <p>Asynchronous working station!</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
