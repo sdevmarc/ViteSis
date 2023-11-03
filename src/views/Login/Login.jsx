bf18fb1925151eda57350d28ee0e31b142ee309f
Marc Edison <suanieeee@yahoo.com>
2023-11-04 01:25:07 +0800

    asdasd

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import Header from '../../components/Header/Header'
 import './style.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+    const Navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3004/login', { username, password })
+            .then(result => {
+                console.log(result)
+                if(result.data === 'Success') {
+                    Navigate('/dashboard')
+                }
+            })
+
+            .catch(err => console.log(err))
+    }
     return (
         <div className="App">
             <Header />
             <section className="section-1">
                 <div className="login-form">
-                    <form action="/dashboard">
+                    <form onSubmit={handleSubmit}>
                         <h1>Login</h1>
-                        <input type="text" placeholder='Username' required />
-                        <input type="password" placeholder='Password' required />
+                        <input type="text" placeholder='Username' onChange={(e) => setUsername(e.target.value)} required />
+                        <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} required />
                         <button type="submit">
                             Login
                         </button>
