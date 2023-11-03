b79595015e4e102ed373e6d38c13b23505286010
Marc Edison <suanieeee@yahoo.com>
2023-11-03 20:09:14 +0800

    login form kinda goods

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import './style.css'
 
 const Login = () => {
     return (
         <div className="App">
             <Header />
+            <section className="section-1">
+                <div className="login-form">
+                    <form action="/">
+                        <h1>Login</h1>
+                        <input type="email" placeholder='Username' required />
+                        <input type="password" placeholder='Password' required />
+                        <button type="submit">
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </section>
         </div>
     )
 }
